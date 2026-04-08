7328f01e739084584589d95a2daa78f920b29ddc
yashwantgursal6498-sketch <yashwantgursal6498@gmail.com>
2026-04-08 10:57:39 +0530

    Create Component.js

---
+++ Webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/fe/core/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("yey.partner360ui.partner360v4.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
